205a15148b318f53eb7aed56780b41cbf2f89e13
shomsy <milosharkor@hotmail.com>
2022-03-01 04:19:03 +0100

    New migration file. Added 'company' in users table.

---
+++ database/migrations/2022_03_01_031109_add_github_users_company_name_field.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // company in GitHub acc. is optional so this should be nullable
+            $table->string('company')->after('remember_token')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('company');
+        });
+    }
+};

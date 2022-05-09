766dff40f290e632fabd6304d6a598a4c24d1e2d
shomsy <milosharkor@hotmail.com>
2022-05-10 01:36:47 +0200

    - Prepare for unit tests: Removed example tests. Configured phpunit.xml and env.testing.example. Made UserFactory for tests.

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory
  */
 class UserFactory extends Factory
 {
@@ -15,7 +15,7 @@ class UserFactory extends Factory
      *
      * @return array<string, mixed>
      */
-    public function definition()
+    public function definition(): array
     {
         return [
             'name' => $this->faker->name(),
@@ -23,6 +23,10 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
+            'company' => 'My Awesome Company',
+            'github_id' => 1,
+            'github_token' => 'foo',
+            'github_avatar' => 'https://en.gravatar.com/userimage',
         ];
     }
 
@@ -31,7 +35,7 @@ class UserFactory extends Factory
      *
      * @return static
      */
-    public function unverified()
+    public function unverified(): static
     {
         return $this->state(function (array $attributes) {
             return [
